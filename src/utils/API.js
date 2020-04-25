dd89d20c100fbe60d1d7a880c5591c64616afa8a
Kenny Lam <kinwai.lam730@gmail.com>
2020-04-25 01:00:07 -0400

    Added a news headline call in API

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,9 @@ import axios from "axios"
 export default {
   getCurrentCovid: function(){
     return axios.get("https://covidtracking.com/api/states")
+  },
+
+  getTopHeadlines: function(){
+    return axios.get("https://newsapi.org/v2/everything?q=COVID&from=2020-03-16&sortBy=publishedAt&apiKey=&pageSize=100&page=2")
   }
-
-
-}   
+} 
