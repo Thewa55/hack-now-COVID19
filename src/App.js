e6f384bb67cfb734d3a29bfc7ec57ab9d6c2e63b
Bryan Teng <bteng1226@gmail.com>
2020-04-24 23:31:01 -0500

    added semantic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Button, Icon} from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
       landing page
+      <Button size="small" color="green">
+        <Icon name="download" />
+        Download
+      </Button>
     </div>
   );
 }
