0a2eced98b2b331205d3aa70007515dd3ae92fdc
Henry Ni <henryni914@gmail.com>
2020-04-25 17:57:55 -0400

    added a menu dropdown and menu items

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,19 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import API from "./utils/API";
 import StateCard from './components/StateCard';
+import StateMenu from './components/StateMenu';
 import MapContainer from './components/MapContainer'
-import Header from "./components/Header"
-import Modal from "./components/Modal"
+import Header from './components/Header'
+import Modal from './components/Modal'
+import StateDropdown from './components/StateDropdown'
 
 function App() {
 
   const [states, setStates] = useState([])
   const [headlines, setHeadlines] = useState([])
-  const [coords, setCoords] = useState({lat: 0, lng: 0})
+  const [coords, setCoords] = useState({ lat: 0, lng: 0 })
 
-  function getStates(){
+  function getStates() {
     API.getCurrentCovid()
       .then(res => {
         setStates(res.data)
@@ -20,13 +22,13 @@ function App() {
   }
 
   function position() {
-     navigator.geolocation.getCurrentPosition(
+    navigator.geolocation.getCurrentPosition(
       position => {
-          setCoords({ 
-          lat: position.coords.latitude, 
+        setCoords({
+          lat: position.coords.latitude,
           lng: position.coords.longitude
         })
-      }, 
+      },
       err => console.log(err)
     );
   }
@@ -53,10 +55,11 @@ function App() {
     <div className="App">
       <Header />
       <div className="Map">
-        <MapContainer coords={coords}/>
+        <MapContainer coords={coords} />
       </div>
-      <Modal />
-      <div class="ui container">
+      {/* <Modal /> */}
+      <StateMenu state={states}/>
+      {/* <div class="ui container">
         <div class="ui three stackable cards">
           {states.map(element => (
             <StateCard
@@ -64,14 +67,17 @@ function App() {
             />
           ))}
         </div>
-      </div>
+      </div> */}
+      {/* <StateDropdown state={states} /> */}
+
       <h3>World Map</h3>
-      <iframe width="650" height="400" frameborder="0" scrolling="no" 
-        marginheight="0" marginwidth="0" title="2019-nCoV" 
+      <iframe width="650" height="400" frameborder="0" scrolling="no"
+        marginheight="0" marginwidth="0" title="2019-nCoV"
         src="//arcgis.com/apps/Embed/index.html?webmap=14aa9e5660cf42b5b4b546dec6ceec7c&extent=77.3846,11.535,163.5174,52.8632&zoom=true&previewImage=false&scale=true&disable_scroll=true&theme=light"
       ></iframe>
+
       <h3>US Map</h3>
-      <iframe style={{width: "1300px", height: "800px", overflow: "hidden"}} src="https://www.arcgis.com/apps/opsdashboard/index.html#/409af567637846e3b5d4182fcd779bea" width="100" height="100" scrolling="no">Iframes not supported</iframe>
+      <iframe style={{ width: "1300px", height: "800px", overflow: "hidden" }} src="https://www.arcgis.com/apps/opsdashboard/index.html#/409af567637846e3b5d4182fcd779bea" width="100" height="100" scrolling="no">Iframes not supported</iframe>
       <div>Maps provided by Johns Hopkins University</div>
     </div>
   );
---
+++ client/src/components/StateMenu.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import { Grid, Menu, Segment, Dropdown } from 'semantic-ui-react'
+
+export default function StateMenu(props) {
+
+    const [active, setActive] = useState("cases")
+    const [currentState, setCurrentState] = useState("")
+
+    function handleItemClick(e, { name }) {
+        setActive(name)
+    };
+
+    const allStates = props.state
+    const stateOptions = allStates.map(element => (
+        {
+            key: element.state,
+            text: element.state,
+            value: element.state,
+            name: element.state,
+            // onClick: function handleStateClick(e, {name}) {
+            //     setCurrentState(name)
+            // }
+        }
+    ))
+
+    console.log('currentState: ',currentState)
+    console.log('active: ', active)
+
+    return (
+
+        <Grid>
+            <Grid.Column width={4}>
+                <Menu fluid vertical tabular>
+                <Dropdown pointing='left' className='link item' placeholder='State' search selection options={stateOptions}/>
+                    {/* <Dropdown placeholder='States' pointing='left' className='link item' text={currentState}>
+                        <Dropdown.Menu>
+                            {allStates.map(element => (
+                                <Dropdown.Item key={element.state} name={element.state} onClick={handleStateClick}>{element.state} </Dropdown.Item>
+                            ))}
+                        </Dropdown.Menu>
+                    </Dropdown> */}
+                    <Menu.Item
+                        name='cases'
+                        active={active === 'cases'}
+                        onClick={handleItemClick}
+                    />
+                    <Menu.Item
+                        name='resources'
+                        active={active === 'resources'}
+                        onClick={handleItemClick}
+                    />
+                    <Menu.Item
+                        name='empty'
+                        active={active === 'empty'}
+                        onClick={handleItemClick}
+                    />
+                    <Menu.Item
+                        name='links'
+                        active={active === 'links'}
+                        onClick={handleItemClick}
+                    />
+                </Menu>
+            </Grid.Column>
+
+            <Grid.Column stretched width={12}>
+                <Segment>
+                    This is an stretched grid column. This segment will always match the
+                    tab height
+          </Segment>
+            </Grid.Column>
+        </Grid>
+
+    )
+}
