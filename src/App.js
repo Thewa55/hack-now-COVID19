962c9ca6be3833a6f2361e2f5f5d319d025e03e6
Kenny Lam <kinwai.lam730@gmail.com>
2020-04-25 00:40:23 -0400

    Added basic API call to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import {Button, Icon} from 'semantic-ui-react'
+import API from "../../utils/API"
 
 function App() {
+
+  const [states, setStates] = useState([])
+
+  function getStates(){
+    API.getCurrentCovid()
+      .then(res => {
+        setStates(res.data)
+      })
+  }
+
+  useEffect(() => {
+    getStates()
+  }, [])
+
+  console.log(states)
   return (
     <div className="App">
       landing page
