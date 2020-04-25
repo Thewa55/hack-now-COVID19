34086d5d7ec34f0a2f76bd841a7c8e1382edfffe
Kenny Lam <kinwai.lam730@gmail.com>
2020-04-25 19:23:17 -0400

    Updated sign in page

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -1,39 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Input, Button } from 'semantic-ui-react'
 
-const Signup = () => (
-  <Form>
-    <Form.Group widths='equal'>
-      <Form.Field
-        id='form-input-control-first-name'
-        control={Input}
-        label='First name'
-        placeholder='First name'
-      />
-      <Form.Field
-        id='form-input-control-last-name'
-        control={Input}
-        label='Last name'
-        placeholder='Last name'
-      />
-    </Form.Group>
-    <Form.Field
-      id='form-input-control-error-email'
-      control={Input}
-      label='Email'
-      placeholder='joe@schmoe.com'
-      error={{
-        content: 'Please enter a valid email address',
-        pointing: 'below',
-      }}
-    />
-    <Form.Field
-      id='form-button-control-public'
-      control={Button}
-      content='Confirm'
-      label='Label with htmlFor'
-    />
-  </Form>
-)
+function Signup() {
+  const [checks,setChecks] = useState(true)
+  const [member, setMembet] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    phonenumber: "",
+    password: ""
+  })
 
-export default Signup
+  function handleInputChange(event){
+    const {name, value} = event.target;
+    
+  }
+
+  return(
+    <Form>
+      <Form.Group widths='equal'>
+        <Form.Field
+          id='form-input-control-first-name'
+          control={Input}
+          label='First name'
+          placeholder='First name'
+          name='firstname'
+          onChange={handleInputChange}
+        />
+        <Form.Field
+          id='form-input-control-last-name'
+          control={Input}
+          label='Last name'
+          placeholder='Last name'
+          name='lastname'
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      <Form.Group widths='equal'>
+        <Form.Field
+          id='form-input-control-error-email'
+          control={Input}
+          label='Email'
+          placeholder='joe@schmoe.com'
+          name='email'
+          onChange={handleInputChange}
+        />
+        <Form.Field
+          id=''
+          control={Input}
+          label='Phone Number (optional)'
+          placeholder='1-888-888-8888'
+          name='phonenumber'
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      <Form.Group widths='equal'>
+        <Form.Field
+          control={Input}
+          label='Password'
+          placeholder='Password'
+          name='password1'
+          type='password'
+          onChange={handleInputChange}
+        />
+        <Form.Field
+          control={Input}
+          label='Confirm Password'
+          placeholder='Confirm Password'
+          name='password2'
+          type='password'
+          onChange={handleInputChange}
+        />
+      </Form.Group>
+      <Form.Field
+        id='form-button-control-public'
+        disabled={checks}
+        control={Button}
+        content='Submit'
+      />
+    </Form>
+  )
+}
+
+export default Signup;
