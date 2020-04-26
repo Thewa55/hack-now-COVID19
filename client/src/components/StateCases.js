d254d04825e97933cb6fd563db22b9c2df7b4240
Henry Ni <henryni914@gmail.com>
2020-04-25 21:44:11 -0400

    created positive cases, total tests, recovered

--- client/src/components/StateCases.js
+++ client/src/components/StateCases.js
@@ -4,28 +4,38 @@ import StateAbbreviations from '../utils/StateAbbreviations.json'
 
 export default function StateCases(props) {
 
-    // console.log('statecases: ', props.info[0])
     let stateObj = props.info;
     console.log('stateObj ', stateObj)
-    let activeStates = props.activeState;
-    console.log('activeState, ', activeStates)
-    let spelledState = StateAbbreviations.find(element => element.abbreviation === activeStates);
-    console.log('spelledState: ', spelledState)
+    let activeState = props.activeState;
+    // console.log('activeState, ', activeState)
+    let spelledState = StateAbbreviations.find(element => element.abbreviation === activeState);
+    // console.log('spelledState: ', spelledState)
 
     return (
 
 
         <List>
-            {spelledState ? 
-            <>
-            <h2>{spelledState.name} ({activeStates})</h2>
-            <List.Item>
-                <List.Header>Positive Cases: {props.info.positive}</List.Header>
-                These are the number of tests that came back positive
-            </List.Item>
-            </>
-            : <h2>Here are the US totals</h2> }
-            
+            {spelledState ?
+                <>
+                    <h2>{spelledState.name} ({activeState})</h2>
+                    <List.Item>
+
+                        {stateObj.map(element => (
+                            // console.log(element.positive)
+                            <>
+                                <List.Header>Positive Cases: {element.positive}</List.Header>
+                                <p>These are the number of tests that came back positive.</p>
+                                <List.Header>Total Tests: {element.totalTestResults}</List.Header>
+                                <p>These are the total number of tests.</p>
+                                <List.Header>Recovered: {element.recovered == null ? "N/A" : element.recovered }</List.Header>
+                                <p>These are the total number of people that have recovered from COVID-19.</p>
+                            </>
+                        ))}
+
+                    </List.Item>
+                </>
+                : <h2>Here are the US totals</h2>}
+
         </List>
     )
 }
